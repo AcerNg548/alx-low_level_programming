ede369ef695d6d847547abaeb039dc3918089db4
AcerNg548 <105017838+AcerNg548@users.noreply.github.com>
2022-06-08 23:59:38 +0100

    betty

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * jack_bauer - a function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * jack_bauer - A function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
  * h = hour, m = minutes
  * / 10 allows second digit to rotate
  * for loop breaks before passing 24:00
