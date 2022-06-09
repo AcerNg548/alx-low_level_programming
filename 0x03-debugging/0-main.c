c4691fbc003063c2e7726c80ff4430beefae2e70
Paulo <acerpaulo4@gmail.com>
2022-06-09 00:03:59 -0700

    lesson learnt

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Print a random number and state whether
+ *It is positive, negative, or zero
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,9 @@
+#ifdef main_h
+#define main_h
+
+#include <stdio.h>
+
+void positive_or_negative(int n);
+
+
+#endif /* main_h */
