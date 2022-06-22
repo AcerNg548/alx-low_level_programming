8645c2e3f1d8177ce95723dcbb689a490d104d26
AcerNg548 <105017838+AcerNg548@users.noreply.github.com>
2022-06-22 22:26:16 +0100

    1.2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,20 +4,11 @@
  * _print_rev_recursion - prints a string in reverse
  * @s: string to be printed
  */
-#include "main.h"
-
-/**
- * _puts_recursion - Prints a string followd by a new line.
- * @s: The string to be printed.
- */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		_puts_recursion(s + 1);
 	}
-
-	else
-		_putchar('\n');
 }
