18969379547da3b17eefcbf3eae9a6ce2386a141
AcerNg548 <105017838+AcerNg548@users.noreply.github.com>
2022-06-22 22:32:37 +0100

    3.1

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -16,11 +16,11 @@ int factorial(int n);
 	{
 		return (-1);
 	}
-	else if (n == 0)
+	else if (n >= 0 && n <= 1)
 	{
 		return (1);
 	}
-	result = n * (n - 1);
+	result *= factorial(n - 1);
 
 	return (result);
 }
