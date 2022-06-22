48185995d8ed9b5a2fdbcf5ce84cb511d3b1a9e6
AcerNg548 <105017838+AcerNg548@users.noreply.github.com>
2022-06-22 22:23:55 +0100

    0.1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _puts_recursion(char *s) - prints a string
- * followed by a new line
- * @s: the string to be printed
+ * _puts_recursion - Prints a string followd by a new line.
+ * @s: The string to be printed.
  */
-void _puts_recursion(char *s);
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
-		_puts_recursions(s + 1);
+		_puts_recursion(s + 1);
 	}
 
 	else
